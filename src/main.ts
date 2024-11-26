50d0851c2c6dfec4c1bf061caf15599d5257adac
Lalit Rajak <lalitrajak312@gmail.com>
2024-11-26 23:49:20 +0530

    setup vercel

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const isServerless = process.env.VERCEL === 'true';
+
+  if (isServerless) {
+    const server = express();
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    await app.init();
+    return server;
+  } else {
+    const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Application is running on: http://localhost:${port}`);
+  }
 }
-bootstrap();
+
+bootstrap().then((server) => {
+  if (server) {
+    module.exports = server;
+  }
+});
